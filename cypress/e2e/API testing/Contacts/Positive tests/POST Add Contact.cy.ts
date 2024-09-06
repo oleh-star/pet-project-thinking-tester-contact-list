5057f9a6dbd4622f7e9d12af9ded1e49cf9c04f6
oleh-star <olezhyks9@gmail.com>
2024-09-06 20:22:23 +0300

    feat: Add API tests for adding a new contact

--- cypress/e2e/API testing/Contacts/Positive tests/POST Add Contact.cy.ts
+++ cypress/e2e/API testing/Contacts/Positive tests/POST Add Contact.cy.ts
@@ -1,26 +1,69 @@
+import { faker } from '@faker-js/faker'
 
-describe('My tests', () => {
+describe('Add new contact', () => {
   let token: string;
+  let contactId: string;
 
   before(() => {
     cy.getToken().then((t: string) => {
       token = t;
-      cy.log("token = ", token);
-    });
+      cy.log(`token: ${token}`);
+    })
   });
 
-  it('should make a request with the token', () => {
+  it('Add new contact', () => {
     cy.request({
-      method: 'GET',
+      method: 'POST',
       url: '/contacts',
       headers: {
         Authorization: `Bearer ${token}`
+      },
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
       }
     }).then((response) => {
-      expect(response.status).to.eq(200);
-
-      expect(response.body[0]).to.have.property('_id');
-      expect(response.body[0]).to.have.property('firstName', "Johdfsn");
+      contactId = response.body._id
+      expect(response.status).to.eq(201);
+      expect(response.body.firstName).is.not.empty;
+      expect(response.body.lastName).is.not.empty;
+      expect(response.body.birthdate).is.not.empty;
+      expect(response.body.email).is.not.empty;
+      expect(response.body.phone).to.have.length(10);
+      expect(response.body.street1).is.not.empty;
+      expect(response.body.street2).is.not.empty;
+      expect(response.body.city).is.not.empty;
+      expect(response.body.stateProvince).is.not.empty;
+      expect(response.body.postalCode).is.not.empty;
+      expect(response.body.country).is.not.empty;
+      expect(response.body.owner).is.not.empty;
+      expect(response.body.__v).eq(0);
     })
+  });
+
+  afterEach(() => {
+    if (contactId) {
+      cy.request({
+        method: 'DELETE',
+        url: `/contacts/${contactId}`,
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        cy.log('Contact are deleted')
+        console.log('Contact are deleted')
+      });
+    }
   });
 });
