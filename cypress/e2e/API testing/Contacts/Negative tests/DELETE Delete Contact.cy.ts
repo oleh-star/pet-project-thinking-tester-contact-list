bb8f8a7ebd12a421ee45374fc6d99990b17910bf
oleh-star <olezhyks9@gmail.com>
2024-09-09 15:31:15 +0300

    Add negative DELETE and POST tests

--- cypress/e2e/API testing/Contacts/Negative tests/DELETE Delete Contact.cy.ts
+++ cypress/e2e/API testing/Contacts/Negative tests/DELETE Delete Contact.cy.ts
@@ -1,22 +1,96 @@
-describe('Login and get token', () => {
-  it('API Login test', () => {
-    cy.request(
-      "POST",
-      "/users/login",
-      {
-        "email": "mynana1057@fak.com",
-        "password": "mynana1057@fak.com"
-      }).then((response) => {
-        const token = response.body.token;
-        Cypress.env('authToken', token);
-        console.log(token);
-        expect(response.status).to.eq(200);
-        expect(response.body).to.have.property('token', token);
-        expect(response.body).to.have.property('user');
-        expect(response.body.user).to.have.property('_id', '66d19bb3e58d2200130d9bf8');
-        expect(response.body.user).to.have.property('firstName', 'Michael');
-        expect(response.body.user).to.have.property('lastName', 'Chypapa');
+describe('Delete user', () => {
+  
+  let token: string;
+  let contactId: string;
+  
+  before(() => {
+    cy.getToken().then((t: string) => {
+      token = t;
+      cy.log(`token: ${token}`);
+    });
+    cy.getContactId().then((с: string) => {
+      contactId = с;
+      cy.log(`contactId: ${contactId}`);
+    })
+  });
 
-      })
+  it('Should return 401 when token is invalid', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/contacts/${contactId}`,
+      headers: {
+        Authorization: `Bearer token`
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+      expect(response.body).to.have.property('error', 'Please authenticate.');
+    })
   })
-})
+
+  it('Should return 401 when token is missing', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/contacts/${contactId}`,
+      headers: {
+        Authorization: `Bearer `
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+      expect(response.body).to.have.property('error', 'Please authenticate.');
+    })
+  })
+
+  it('Should return 400 when contactId does not exist', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/contacts/contactId`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.eq('Invalid Contact ID');
+    })
+  })
+
+  it('Should return 503 when contactId is missing', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/contacts/`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      timeout: 60000
+    }).then((response) => {
+      expect(response.status).to.eq(503);
+    })
+  })
+
+  it('Should return 404 when trying to delete an already deleted contact', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/contacts/${contactId}`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+
+    cy.request({
+      method: 'DELETE',
+      url: `/contacts/${contactId}`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+    });
+  });
+});
--- cypress/e2e/API testing/Contacts/Negative tests/POST Add Contact.cy.ts
+++ cypress/e2e/API testing/Contacts/Negative tests/POST Add Contact.cy.ts
@@ -1,26 +1,378 @@
+import { faker } from '@faker-js/faker'
 
-describe('My tests', () => {
+describe('Add new contact', () => {
   let token: string;
+  let contactId: string;
 
   before(() => {
     cy.getToken().then((t: string) => {
       token = t;
-      cy.log("token = ", token);
-    });
+      cy.log(`token: ${token}`);
+    })
   });
 
-  it('should make a request with the token', () => {
+  it('Should return 401 when token is missing', () => {
     cy.request({
-      method: 'GET',
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer `
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+      expect(response.body).to.have.property('error', 'Please authenticate.');
+    })
+  });
+
+  it('Should return 401 when token is invalid', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer token`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(401);
+      expect(response.body).to.have.property('error', 'Please authenticate.');
+    })
+  });
+
+  it('Should return 400 when firstName is missing', () => {
+    cy.request({
+      method: 'POST',
       url: '/contacts',
       headers: {
         Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": "",
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
       }
     }).then((response) => {
-      expect(response.status).to.eq(200);
-
-      expect(response.body[0]).to.have.property('_id');
-      expect(response.body[0]).to.have.property('firstName', "Johdfsn");
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: firstName: Path `firstName` is required.');
     })
   });
+
+  it('Should return 400 when all field are missed', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": "",
+        "lastName": "",
+        "birthdate": "",
+        "email": "",
+        "phone": "",
+        "street1": "",
+        "street2": "",
+        "city": "",
+        "stateProvince": "",
+        "postalCode": "",
+        "country": ""
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: firstName: Path `firstName` is required., lastName: Path `lastName` is required., birthdate: Birthdate is invalid, email: Email is invalid, phone: Phone number is invalid, postalCode: Postal code is invalid');
+    })
+  });
+
+  it('Should return 400 when email format is invalid', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": "invalidEmailFormat",
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: email: Email is invalid');
+    })
+  });
+
+  it('Should return 400 when phone number is too short', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 5 }),//6 minimum valid length
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: phone: Phone number is invalid');
+    })
+  });
+
+  it('Should return 400 when phone number is too long', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 12 }),//11 maximum valid length
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: phone: Phone number is invalid');
+    })
+  });
+
+  it('Should return 400 when firstName is too long', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        "firstName": faker.string.alpha({ length: 256 }), // very long name
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      },
+
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('_message', 'Contact validation failed');
+    })
+  });
+
+  it('Should return 400 when birthdate format is invalid', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": 'invalid-date',
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: birthdate: Birthdate is invalid');
+    })
+  });
+
+  it('Should fail when birthdate is in the future', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": '2100-01-01',
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.have.property('message', 'Contact validation failed: birthdate: Birthdate is invalid');
+    })
+  });
+
+  it('Should return 400 when postalCode is too short', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 2 }), //3 minimum valid length
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).have.property('message', 'Contact validation failed: postalCode: Postal code is invalid');
+    })
+  });
+
+  it('Should return 400 when postalCode is too long', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": faker.internet.email(),
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 8 }), //7 maximum valid length
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).have.property('message', 'Contact validation failed: postalCode: Postal code is invalid');
+    })
+  });
+
+  it('Should return 400 when email already exists', () => {
+    cy.request({
+      method: 'POST',
+      url: '/contacts',
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      failOnStatusCode: false,
+      body: {
+        // automation generating of random credentials
+        "firstName": faker.person.firstName(),
+        "lastName": faker.person.lastName(),
+        "birthdate": faker.date.birthdate().toISOString().split('T')[0],
+        "email": 'glennie_robel95@hotmail.com', //this email already exist
+        "phone": faker.string.numeric({ length: 10 }),
+        "street1": faker.location.streetAddress(),
+        "street2": faker.location.streetAddress(),
+        "city": faker.location.city(),
+        "stateProvince": faker.location.state({ abbreviated: true }),
+        "postalCode": faker.string.numeric({ length: 5 }),
+        "country": faker.location.countryCode()
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).have.property('message', 'Contact validation failed: email: email is invalid');
+    })
+  });
+
 });
